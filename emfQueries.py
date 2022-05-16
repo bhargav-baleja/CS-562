aa2fff211d022395f36905bebc67c513422bc06f
bhargav-baleja <bhargav.baleja.11@gmail.com>
2022-05-16 13:53:11 -0400

    Added some comments

--- emfQueries.py
+++ emfQueries.py
@@ -1,5 +1,5 @@
 function = """
-
+#Function for setting attribute value according to the input provided
 def getAttributesValue(attr): 
 	if attr == "cust":
 		flag = 0
@@ -22,8 +22,10 @@ def getAttributesValue(attr):
 
 predicates = predicates.split(',') #splits predicates by each predicate statment and creates list to store the parts of each predicate in a single 2D array
 pList = []
+
 for i in predicates:
 	pList.append(i.split(' '))
+
 for i in range(int(groupingVarCount)+1):
     # 0th pass of the algorithm, where each row of the MF Struct is initalized for every unique group based on the grouping variables.
     # Each row in the MF struct also has its columns initalized appropriately based on the aggregates in the F-Vect
@@ -31,14 +33,17 @@ for i in range(int(groupingVarCount)+1):
 		for row in query:
 			key = ''
 			value = {}
+			
 			for attr in groupingAttributes.split(','):
 				flag = getAttributesValue(attr) 
 				key += f'{str(row[flag])},'
 			key = key[:-1]
+			
 			if key not in MF_Struct.keys():
 				for groupAttr in groupingAttributes.split(','):
 					flag = getAttributesValue(groupAttr) 
 					colVal = row[flag]
+					
 					if colVal:
 						value[groupAttr] = colVal
 				for fVectAttr in fVect.split(','):
@@ -58,20 +63,23 @@ for i in range(int(groupingVarCount)+1):
 			groupVar = aggList[0]
 			aggFunc = aggList[1]
 			aggCol = aggList[2]
-            # Check to make sure the aggregate function is being called on the grouping variable you are currently on (i)
+            
+			# Check to make sure the aggregate function is being called on the grouping variable you are currently on (i)
             # Also loop through every key in the MF_Struct to update every row of the MF_Struct the predicate statments apply to(1.state = state and 1.cust = cust vs 1.state = state)
 			if i == int(groupVar):
 				for row in query:
 					for key in MF_Struct.keys():
 						if aggFunc == 'sum':
 							evalString = predicates[i-1]
-                            # Creates a list1 to be run with the eval() method by replacing grouping variables with their actual values
+                            
+							# Creates a list1 to be run with the eval() method by replacing grouping variables with their actual values
                             # Since it's an EMF query, it must also check if the list1 is a grouping variable and replace that with the actual value from the table row as well
 							for list1 in pList[i-1]:
 								if len(list1.split('.')) > 1 and list1.split('.')[0] == str(i):
 									new_string = list1.split('.')[1]
 									flag = getAttributesValue(new_string)
 									rowVal = row[flag]
+									
 									try:
 										int(rowVal)
 										evalString = evalString.replace(list1, str(rowVal))
@@ -79,6 +87,7 @@ for i in range(int(groupingVarCount)+1):
 										evalString = evalString.replace(list1, f"'{rowVal}'")
 								elif list1 in groupingAttributes.split(','):
 									rowVal = MF_Struct[key][list1]
+									
 									try:
 										int(rowVal)
 										evalString = evalString.replace(list1, str(rowVal))
@@ -93,11 +102,13 @@ for i in range(int(groupingVarCount)+1):
 							sum = MF_Struct[key][aggregate]['sum']
 							count = MF_Struct[key][aggregate]['count']
 							evalString = predicates[i-1]
+							
 							for list1 in pList[i-1]:
 								if len(list1.split('.')) > 1 and list1.split('.')[0] == str(i):
 									new_string = list1.split('.')[1]
 									flag = getAttributesValue(new_string)
 									rowVal = row[flag]
+									
 									try:
 										int(rowVal)
 										evalString = evalString.replace(list1, str(rowVal))
@@ -105,25 +116,30 @@ for i in range(int(groupingVarCount)+1):
 										evalString = evalString.replace(list1, f"'{rowVal}'")
 								elif list1 in groupingAttributes.split(','):
 									rowVal = MF_Struct[key][list1]
+									
 									try:
 										int(rowVal)
 										evalString = evalString.replace(list1, str(rowVal))
 									except:
 										evalString = evalString.replace(list1, f"'{rowVal}'")
-                            # If evalString is true and count isn't 0, update the avg
+                            
+							# If evalString is true and count isn't 0, update the avg
 							if eval(evalString.replace('=', '==')):
 								flag = getAttributesValue(aggCol)
 								sum += int(row[flag])
 								count += 1
+								
 								if count != 0:
 									MF_Struct[key][aggregate] = {'sum': sum, 'count': count, 'avg': (sum/count)}
 						elif aggFunc == 'min':
 							evalString = predicates[i-1]
+							
 							for list1 in pList[i-1]:
 								if len(list1.split('.')) > 1 and list1.split('.')[0] == str(i):
 									new_string = list1.split('.')[1]
 									flag = getAttributesValue(new_string)
 									rowVal = row[flag]
+									
 									try:
 										int(rowVal)
 										evalString = evalString.replace(list1, str(rowVal))
@@ -131,6 +147,7 @@ for i in range(int(groupingVarCount)+1):
 										evalString = evalString.replace(list1, f"'{rowVal}'")
 								elif list1 in groupingAttributes.split(','):
 									rowVal = MF_Struct[key][list1]
+									
 									try:
 										int(rowVal)
 										evalString = evalString.replace(list1, str(rowVal))
@@ -140,15 +157,18 @@ for i in range(int(groupingVarCount)+1):
 							if eval(evalString.replace('=', '==')):
 								min = int(MF_Struct[key][aggregate])
 								flag = getAttributesValue(aggCol)
+								
 								if int(row[flag]) < min:
 									MF_Struct[key][aggregate] = row[flag]
 						elif aggFunc == 'max':
 							evalString = predicates[i-1]
+							
 							for list1 in pList[i-1]:
 								if len(list1.split('.')) > 1 and list1.split('.')[0] == str(i):
 									new_string = list1.split('.')[1]
 									flag = getAttributesValue(new_string)
 									rowVal = row[flag]
+									
 									try:
 										int(rowVal)
 										valString = evalString.replace(list1, str(rowVal))
@@ -156,6 +176,7 @@ for i in range(int(groupingVarCount)+1):
 										evalString = evalString.replace(list1, f"'{rowVal}'")
 								elif list1 in groupingAttributes.split(','):
 									rowVal = MF_Struct[key][list1]
+									
 									try:
 										int(rowVal)
 										valString = evalString.replace(list1, str(rowVal))
@@ -165,15 +186,18 @@ for i in range(int(groupingVarCount)+1):
 							if eval(evalString.replace('=', '==')):
 								max = int(MF_Struct[key][aggregate])
 								flag = getAttributesValue(aggCol)
+								
 								if int(row[flag]) > max:
 									MF_Struct[key][aggregate] = row[flag]
 						elif aggFunc == 'count':
 							evalString = predicates[i-1]
+							
 							for list1 in pList[i-1]:
 								if len(list1.split('.')) > 1 and list1.split('.')[0] == str(i):
 									new_string = list1.split('.')[1]
 									flag = getAttributesValue(new_string)
 									rowVal = row[flag]
+									
 									try:
 										int(rowVal)
 										evalString = evalString.replace(list1, str(rowVal))
@@ -181,6 +205,7 @@ for i in range(int(groupingVarCount)+1):
 										evalString = evalString.replace(list1, f"'{rowVal}'")
 								elif list1 in groupingAttributes.split(','):
 									rowVal = MF_Struct[key][list1]
+									
 									try:
 										int(rowVal)
 										evalString = evalString.replace(list1, str(rowVal))
@@ -189,12 +214,15 @@ for i in range(int(groupingVarCount)+1):
                             # If evalString is true, increment the count
 							if eval(evalString.replace('=', '==')):
 								MF_Struct[key][aggregate] += 1
+
 #Generate output table(also checks the HAVING condition)
 output = PrettyTable()
 output.field_names = selectAttributes.split(',')
+
 for row in MF_Struct:
     #create an evalString to be used to check each having condition
 	evalString = ''
+	
 	if havingCondition != '':
 		for list1 in havingCondition.split(' '):
             #if there is a having condition, loop through each element of the having condition to fill in the correct information into the evalString
@@ -211,8 +239,10 @@ for row in MF_Struct:
 						evalString += str(MF_Struct[row][list1])
 			else:
 				evalString += f' {list1} '
+		
 		if eval(evalString.replace('=', '==')):
 			row_info = []
+			
 			for val in selectAttributes.split(','):
 				if len(val.split('_')) > 1 and val.split('_')[1] == 'avg':
 					row_info += [str(MF_Struct[row][val]['avg'])]
@@ -223,14 +253,19 @@ for row in MF_Struct:
 	else:
         #there is no having condition, thus every MFStruct row will be added to the output table
 		row_info = []
+		
 		for val in selectAttributes.split(','):
 			if len(val.split('_')) > 1 and val.split('_')[1] == 'avg':
 				row_info += [str(MF_Struct[row][val]['avg'])]
 			else:
 				row_info += [str(MF_Struct[row][val])]
 		output.add_row(row_info)
+
 print(output)"""
 
+"""
+Fuction for generating a emf query based algorithm
+"""
 def emfQuery():
     with open('algorithm.py', 'a') as algorithmFile:
         algorithmFile.write(function)
--- mfQueries.py
+++ mfQueries.py
@@ -1,4 +1,5 @@
 function = """
+#Function for setting attribute value according to the input provided
 def getAttributesValue(attr): 
 	if attr == "cust":
 		flag = 0
@@ -22,8 +23,10 @@ def getAttributesValue(attr):
 predicates = predicates.split(',')
 pList = []
 #splits predicates by each predicate statment and creates list to store the parts of each predicate in a single 2D array
+
 for i in predicates:
 	pList.append(i.split(' '))
+
 for i in range(int(groupingVarCount)+1): # loop through the table to evaluate each grouping variable
     # 0th pass of the algorithm, where each row of the MF Struct is initalized for every unique group based on the grouping variables.
     # Each row in the MF struct also has its columns initalized appropriately based on the aggregates in the F-Vect
@@ -31,16 +34,20 @@ for i in range(int(groupingVarCount)+1): # loop through the table to evaluate ea
 		for row in query:
 			key = ''
 			value = {}
+			
 			for attr in groupingAttributes.split(','):
 				flag = getAttributesValue(attr)
 				key += f'{str(row[flag])},'
 			key = key[:-1]
+			
 			if key not in MF_Struct.keys():
 				for groupAttr in groupingAttributes.split(','):
 					flag = getAttributesValue(groupAttr)
 					colVal = row[flag]
+					
 					if colVal:
 						value[groupAttr] = colVal
+				
 				for fVectAttr in fVect.split(','):
 					if (fVectAttr.split('_')[1] == 'avg'):
                         # Average is saved as an object with the sum, count, and overall average
@@ -52,7 +59,9 @@ for i in range(int(groupingVarCount)+1): # loop through the table to evaluate ea
 					else:
                         # Initalize values of count, sum and max to 0
 						value[fVectAttr] = 0
+				
 				MF_Struct[key] = value #add row into MF Struct
+	
 	else: #The other n passes for each grouping variable 
 			for aggregate in fVect.split(','):
 				aggList = aggregate.split('_')
@@ -63,13 +72,17 @@ for i in range(int(groupingVarCount)+1): # loop through the table to evaluate ea
 				if i == int(groupVar):
 					for row in query:
 						key = ''
+						
 						for attr in groupingAttributes.split(','):
 							flag = getAttributesValue(attr)
 							key += f'{str(row[flag])},'
+						
 						key = key[:-1]
+						
 						if aggFunc == 'sum':
                             # Creates a list1 to be run with the eval() method by replacing grouping variables with their actual values
 							evalString = predicates[i-1]
+							
 							for list1 in pList[i-1]:
 								if len(list1.split('.')) > 1 and list1.split('.')[0] == str(i):
 									new_string = list1.split('.')[1]
@@ -89,11 +102,13 @@ for i in range(int(groupingVarCount)+1): # loop through the table to evaluate ea
 							sum = MF_Struct[key][aggregate]['sum']
 							count = MF_Struct[key][aggregate]['count']
 							evalString = predicates[i-1]
+							
 							for list1 in pList[i-1]:
 								if len(list1.split('.')) > 1 and list1.split('.')[0] == str(i):
 									new_string = list1.split('.')[1]
 									flag = getAttributesValue(new_string)
 									rowVal = row[flag]
+									
 									try:
 										int(rowVal)
 										evalString = evalString.replace(list1, str(rowVal))
@@ -104,16 +119,19 @@ for i in range(int(groupingVarCount)+1): # loop through the table to evaluate ea
 								flag = getAttributesValue(aggCol)
 								sum += int(row[flag])
 								count += 1
+								
 								if count != 0:
 									MF_Struct[key][aggregate] = {'sum': sum, 'count': count, 'avg': (sum/count)}
 						elif aggFunc == 'min':
 							# check if row meets predicate requirements
 							evalString = predicates[i-1]
+							
 							for list1 in pList[i-1]:
 								if len(list1.split('.')) > 1 and list1.split('.')[0] == str(i):
 									new_string = list1.split('.')[1]
 									flag = getAttributesValue(new_string)
 									rowVal = row[flag]
+									
 									try:
 										int(rowVal)
 										evalString = evalString.replace(list1, str(rowVal))
@@ -123,11 +141,13 @@ for i in range(int(groupingVarCount)+1): # loop through the table to evaluate ea
 							if eval(evalString.replace('=','==')):
 								min = int(MF_Struct[key][aggregate])
 								flag = getAttributesValue(aggCol)
+								
 								if int(row[flag]) < min:
 									MF_Struct[key][aggregate] = row[flag]
 						elif aggFunc == 'max':
 							# check if row meets predicate requirements
 							evalString = predicates[i-1]
+							
 							for list1 in pList[i-1]:
 								if len(list1.split('.')) > 1 and list1.split('.')[0] == str(i):
 									new_string = list1.split('.')[1]
@@ -142,16 +162,19 @@ for i in range(int(groupingVarCount)+1): # loop through the table to evaluate ea
 							if eval(evalString.replace('=','==')):
 								max = int(MF_Struct[key][aggregate])
 								flag = getAttributesValue(aggCol)
+								
 								if int(row[flag]) > max:
 									MF_Struct[key][aggregate] = row[flag]
 						elif aggFunc == 'count':
 							# check if row meets predicate requirements
 							evalString = predicates[i-1]
+							
 							for list1 in pList[i-1]:
 								if len(list1.split('.')) > 1 and list1.split('.')[0] == str(i):
 									new_string = list1.split('.')[1]
 									flag = getAttributesValue(new_string)
 									rowVal = row[flag]
+									
 									try:
 										int(rowVal)
 										evalString = evalString.replace(list1, str(rowVal))
@@ -159,11 +182,14 @@ for i in range(int(groupingVarCount)+1): # loop through the table to evaluate ea
 										evalString = evalString.replace(list1, f"'{rowVal}'")
 							if eval(evalString.replace('=','==')): # If evalString is true, increment the count
 								MF_Struct[key][aggregate] += 1
+
 #Generate output table(also checks the HAVING condition)
 output = PrettyTable()
 output.field_names = selectAttributes.split(',')
+
 for row in MF_Struct:
 	evalString = ''
+	
 	if havingCondition != '':
         #if there is a having condition, loop through each element of the having condition to fill in the correct information into the evalString
         #the eval list1 will be equal to the having condition, replaced with the values of the variables in question,
@@ -182,23 +208,31 @@ for row in MF_Struct:
 				evalString += f' {list1} '
 		if eval(evalString.replace('=','==')):
 			row_info = []
+			
 			for val in selectAttributes.split(','):
 				if len(val.split('_')) > 1 and val.split('_')[1] == 'avg':
 					row_info += [str(MF_Struct[row][val]['avg'])]
 				else:
 					row_info += [str(MF_Struct[row][val])]
 			output.add_row(row_info)
+
 		evalString = ''
 	else:
         #there is no having condition, thus every MFStruct row will be in the output table
 		row_info = []
+		
 		for val in selectAttributes.split(','):
 			if len(val.split('_')) > 1 and val.split('_')[1] == 'avg':
 				row_info += [str(MF_Struct[row][val]['avg'])]
 			else:
 				row_info += [str(MF_Struct[row][val])]
 		output.add_row(row_info)
+
 print(output) #Pretty table corresponding to evaluation of query
+"""
+
+"""
+Fuction for generating a mf query based algorithm
 """
 def mfQuery():
     with open('algorithm.py', 'a') as algorithmFile:
